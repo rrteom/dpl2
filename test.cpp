0bbe9b08448e67f8df74b7d1f31f13f1bb64da61
arrteom <artum.medvedev@gmail.com>
2024-06-01 14:28:18 +0300

    new line+ log

--- test.cpp
+++ test.cpp
@@ -16,30 +16,32 @@ int main() {
     // nodes.prepareNodes(koefs);
     Distribution distribution(50, 50, 20, 20,
                  4.8, n_v_x, n_v_y,
-                 1, 2, 0.1,
+                 1, 2, 1,
                  8, 8, 13, 13);
 
 
     TwoDArr n = distribution.getConcentration();
     ofstream n_file;
-    n_file.open("intgr_test/n");
+    n_file.open("intgr_test/n.log");
     for (int i = 1; i <= n.dimx_; i++) {
         for (int j = 1; j <= n.dimy_; j++) {
             n_file << n.at(i, j) << '\t';
         }
         n_file << endl;
     }
+    n_file << endl;
     n_file.close();
 
     TwoDArr tt = distribution.getTemperature();
     ofstream tt_file;
-    tt_file.open("intgr_test/tt");
+    tt_file.open("intgr_test/tt.log");
     for (int i = 1; i <= n.dimx_; i++) {
         for (int j = 1; j <= n.dimy_; j++) {
             tt_file << tt.at(i, j) << '\t';
         }
         tt_file << endl;
     }
+    tt_file << endl;
     tt_file.close();
 
     return 0;
